227f7e5de26b1dba58a0d011a5d9299c56b913bd
Mårten Gustafson <marten.gustafson@gmail.com>
2014-04-13 01:35:02 +0200

    Ugly hacks are also hacks.

--- src/main/java/spoken/resources/SpokenResource.java
+++ src/main/java/spoken/resources/SpokenResource.java
@@ -1,10 +1,13 @@
 package spoken.resources;
 
+import static com.google.common.base.Strings.nullToEmpty;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
 
+import java.net.URI;
 import java.util.Collection;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -22,6 +25,7 @@ import com.twilio.sdk.TwilioRestException;
 import com.twilio.sdk.verbs.Gather;
 import com.twilio.sdk.verbs.Hangup;
 import com.twilio.sdk.verbs.Say;
+import com.twilio.sdk.verbs.TwiMLException;
 import com.twilio.sdk.verbs.TwiMLResponse;
 import com.twilio.sdk.verbs.Verb;
 
@@ -46,32 +50,45 @@ public class SpokenResource {
     }
 
     @GET
-    public Response answer(@QueryParam("From") final String from) throws Exception {
+    public Response answer(@QueryParam("From") final String from,
+                           @QueryParam("more") @DefaultValue("") final String continuation) throws Exception {
         final TwiMLResponse twiml = new TwiMLResponse();
         LOG.info("Incoming call from " + from);
-        twiml.append(greet(from));
+        twiml.append(greet(from, nullToEmpty(continuation).trim().isEmpty()));
         for(final Source source : this.sources) {
             source.say(twiml, from, this.history);
         }
-        final Gather more = new Gather();
-        more.setMethod("GET");
-        more.setAction(UriBuilder.fromResource(SpokenResource.class).queryParam("From", from).build().toString());
-        more.setNumDigits(1);
-        more.setTimeout(2);
-        more.append(swedish("Tryck valfritt nummer för att höra fler nyheter"));
-        twiml.append(more);
+        twiml.append(hearMore(from));
         twiml.append(new Say("kay thanks bye"));
         twiml.append(new Hangup());
         return Response.ok(twiml.toXML()).build();
     }
 
-    private Verb greet(final String number) throws TwilioRestException {
+    private static Gather hearMore(final String from) throws TwiMLException {
+        final URI apa = UriBuilder
+                .fromResource(SpokenResource.class)
+                .queryParam("From", from)
+                .queryParam("more", "yes")
+                .build();
+        final Gather more = new Gather();
+        more.setMethod("GET");
+        more.setAction(apa.toString());
+        more.setNumDigits(1);
+        more.setTimeout(2);
+        more.append(swedish("Tryck valfritt nummer för att höra fler nyheter"));
+        return more;
+    }
+
+    private Verb greet(final String number, final boolean first) throws TwilioRestException {
         if(this.accounts.isRegistred(number).isPresent()) {
             return swedish("Välkommen tillbaka till nyhetsprat...");
         }
         this.accounts.register(number, "-");
         this.accounts.sendWelcomeSms(number);
-        return swedish("Hej och välkommen till nyhetsprat...");
+        if(first) {
+            return swedish("Hej och välkommen till nyhetsprat...");
+        }
+        return swedish(" ");
     }
 
     public static Say swedish(final String message) {
